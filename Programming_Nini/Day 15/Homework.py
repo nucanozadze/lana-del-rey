0fb12be1c8b2d5428066c02990f5014a0eefca70
Nini <138128778+nucanozadze@users.noreply.github.com>
2023-08-29 21:03:40 +0400

    Add files via upload

---
+++ Programming_Nini/Day 15/Homework.py
@@ -0,0 +1,22 @@
+#saqartvelo
+
+country = "საქართველო"
+print(country[2:],country[6:],country[4:])
+
+country = ["საქართველო", "საფრანგეთი", "ბრაზილია", "იტალია", "კანადა"]
+print(country[1:2])
+print(country[1:3])
+print(country[1:4])
+print(country[1:5])
+print(country[1:6])
+print(country[2:3])
+print(country[2:4])
+print(country[2:5])
+print(country[2:6])
+print(country[2:6])
+print(country[3:4])
+print(country[3:5])
+print(country[3:6])
+print(country[4:5])
+print(country[4:6])
+print(country[5:6])
