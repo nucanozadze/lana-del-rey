d18eaae897fb0d0700539246b92b761ebb08de9b
Nini <138128778+nucanozadze@users.noreply.github.com>
2023-09-05 18:33:48 +0400

    Add files via upload

---
+++ Programming_Nini/Day 17/Homework.py
@@ -0,0 +1,36 @@
+#Day 17
+#Homework
+
+#1
+
+import random
+crew_leaders = ['Kruashvili', 'amiranashvili', 'tyeshelashvili', 'janezashvili', 'molodini', 'kereselidze', 'kurtanidze']
+
+print(crew_leaders[4])
+print(crew_leaders[random.randint(0,6)])
+print(crew_leaders[random.randint(0,6)])
+for i in range(1,4):
+    print('winner N',i,'is',random.choice(crew_leaders))
+
+
+#2
+
+crew_members = ['Nozadze', 'janashia','Motiahvili','Tsamalaidze','Katsarava','Mgeladze','Jatchvadze','Gogadze','Gelashvili','Wiklauri','Egvipte','Bochorishvili','Oqropiridze','Gochitashvili','Chochia']
+choice = random.choice(crew_members)
+choice2 = (choice + ' ' + ' is cool<3')
+print(crew_members[4])
+print(crew_members[random.randint(0,6)])
+print(crew_members[random.randint(0,6)])
+for i in range(1,4):
+    print('winner N',i,'is',random.choice(crew_members) + " is cool <3")
+    
+
+#3
+crew_members = ['Nozadze', 'janashia','Motiahvili','Tsamalaidze','Katsarava','Mgeladze','Jatchvadze','Gogadze','Gelashvili','Wiklauri','Egvipte','Bochorishvili','Oqropiridze','Gochitashvili','Chochia']
+#ამას ვერ მივხვდი როგორ გავაკეთო<3
+
+
+
+
+
+
